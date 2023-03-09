fff9daa49542a5dd79ec32f424be2799f09497e4
Cedric Boucher <onebitalpha@gmail.com>
2023-03-08 19:26:02 -0700

    working on it

--- Process_Logger.py
+++ Process_Logger.py
@@ -1,50 +1,59 @@
-######## method of getting all running proccesses, slow AF ########
-"""
-import wmi
-
-f = wmi.WMI()
-
-print("pid Process name")
-
-for process in f.Win32_Process():
-    print(f"{process.ProcessId} {process.Name}")
-print(len(f.Win32_Process()))
-"""
-
-######## method of getting active focused proccess ########
-"""
 import psutil
+import win32process
+import win32gui
+import time
 
-def get_active_window_process():
+
+def get_active_window_process() -> psutil.Process:
+    """
+    returns the Process object of the process
+    that is in focus by the user, or None
+    """
     pid = None
-    import win32process
-    import win32gui
     window = win32gui.GetForegroundWindow()
     pid = win32process.GetWindowThreadProcessId(window)[1]
 
     if pid is not None:
         return psutil.Process(pid)
+    else:
+        return None
 
-print(get_active_window_process())
 
-"""
+def get_active_processes() -> list:
+    """
+    gets all running processes and returns
+    a list of lists, where each element is
+    [process_id, name_of_process, create_time(s)]
+    """
+    # Get a list of process IDs
+    pids = psutil.pids()
+    output = list()
 
-######## method of getting all running processes, better and faster ########
-import psutil
+    # Print the list of process IDs
+    for pid in pids:
+        try:
+            process = psutil.Process(pid)
+            status = process.status()
+            if status == "running":
+                name = process.name()
+                create_time = process.create_time()
 
-# Get a list of process IDs
-pids = psutil.pids()
+                output.append([pid, name, create_time])
+        except psutil.NoSuchProcess:
+            pass
+    
+    return output
 
-# Print the list of process IDs
-for pid in pids:
-    try:
-        process = psutil.Process(pid)
-        name = process.name()
-        status = process.status()
-        memory_info = process.memory_info()
 
-        # Do something with the information
-        print(f"PID {pid}: {name} ({status}) - Memory: {memory_info.rss / (1024*1024)} MB")
-    except psutil.NoSuchProcess:
-        pass
-print(len(pids))
+def append_to_log_file(file = "process_log.csv") -> None:
+    pass
+
+
+def main() -> None:
+    #print(get_active_window_process())
+    print(get_active_processes())
+
+
+if __name__ == "__main__":
+    main()
+
