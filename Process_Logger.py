43cea985d31799610aecf9d145e141f2e593b0a8
Cedric Boucher <onebitalpha@gmail.com>
2023-03-21 17:05:50 -0600

    now checks if user is logged in / computer is lock

--- Process_Logger.py
+++ Process_Logger.py
@@ -1,4 +1,5 @@
 import psutil
+import wmi
 import win32process
 import win32gui
 import datetime
@@ -51,6 +52,42 @@ def get_active_processes() -> list:
     return output
 
 
+def check_user_and_locked() -> list:
+    """
+    checks if computer is locked or unlocked
+    and which user is logged in
+    (a user can be logged in even if computer is locked)
+    returns list: [is_locked (bool), logged_in_user (string) or None]
+    """
+    # Initialize the WMI module
+    c = wmi.WMI()
+
+    # Query for the current user
+    user_query = "SELECT * FROM Win32_ComputerSystem"
+    user_info = c.query(user_query)[0]
+
+    # Check if the user is logged in or not
+    if user_info.UserName == None:
+        # print("No user is currently logged in.")
+        current_user = None
+    else:
+        # print("The current user is:", user_info.UserName)
+        current_user = str(user_info.UserName)
+
+    # Check if the computer is locked or not
+    lock_query = "SELECT * FROM Win32_Process WHERE Name='LogonUI.exe'"
+    lock_info = c.query(lock_query)
+    if len(lock_info) > 0:
+        # print("The computer is currently locked.")
+        is_locked = True
+    else:
+        # print("The computer is currently unlocked.")
+        is_locked = False
+
+    return [is_locked, current_user]
+
+
+
 def append_to_log_file(file = "process_log.csv", last_known_active_processes = [], last_known_active_window_process = []) -> None:
     """
     appends data to log file, only if there is any change in data.
@@ -58,10 +95,11 @@ def append_to_log_file(file = "process_log.csv", last_known_active_processes = [
     as input on the next call
 
     data format in log (csv) file:
-    date(YYYY-MM-DD),time(HH-MM-SS.ZZZZ),PID(int),name(str),focused(bool),start/end,initial_run
+    date(YYYY-MM-DD),time(HH-MM-SS.ZZZZ),PID(int),name(str),focused(bool),is_locked(bool),current_user(str),start/end,initial_run
     """
     active_window_process = get_active_window_process()
     active_processes = get_active_processes()
+    [is_locked, current_user] = check_user_and_locked()
 
     same_processes = (active_processes == last_known_active_processes)
     same_active_process = (active_window_process == last_known_active_window_process)
@@ -78,9 +116,9 @@ def append_to_log_file(file = "process_log.csv", last_known_active_processes = [
             if not same_active_process:
                 # append changed focused process to log
                 if active_window_process == None:
-                    file_object.writelines(["{},{},{},{},{},{},{}\n".format(date, time, active_window_process, active_window_process, True, None, first_run)])
+                    file_object.writelines(["{},{},{},{},{},{},{},{},{}\n".format(date, time, active_window_process, active_window_process, True, is_locked, current_user, None, first_run)])
                 else:
-                    file_object.writelines(["{},{},{},{},{},{},{}\n".format(date, time, active_window_process[0], active_window_process[1], True, None, first_run)])
+                    file_object.writelines(["{},{},{},{},{},{},{},{},{}\n".format(date, time, active_window_process[0], active_window_process[1], True, is_locked, current_user, None, first_run)])
             if not same_processes:
                 # append changed processes to log
                 process_differences = [process for process in active_processes if process not in last_known_active_processes]
@@ -93,7 +131,7 @@ def append_to_log_file(file = "process_log.csv", last_known_active_processes = [
                             start_or_end = "start"
                         else:
                             start_or_end = "end"
-                        output_lines.append("{},{},{},{},{},{},{}\n".format(date, time, process[0], process[1], False, start_or_end, first_run))
+                        output_lines.append("{},{},{},{},{},{},{},{},{}\n".format(date, time, process[0], process[1], False, is_locked, current_user, start_or_end, first_run))
                 file_object.writelines(output_lines)
     
     return [active_processes, active_window_process]
